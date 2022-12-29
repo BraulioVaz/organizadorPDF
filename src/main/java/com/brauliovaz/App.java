7e6c3cae1177e9752312c389167adc600e16e954
Braulio <brauliovaz1220@gmail.com>
2022-12-28 20:18:47 -0600

    Creacion de repositorio maven

---
+++ src/main/java/com/brauliovaz/App.java
@@ -0,0 +1,13 @@
+package com.brauliovaz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
